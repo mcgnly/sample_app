524f0544021d4f2839a6537c56499376a441546e
mcgnly <katie@mcgnly.com>
2015-10-29 13:21:19 +0100

    write test for users page test helper throwing a runtime error I don't understand, so comment out that part

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,11 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+  # test "full title helper" do
+  #   assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+  #   assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+  # end
+
 end
