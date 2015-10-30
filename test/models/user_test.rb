1b9f7c5394c5e558fbf0e8cc10fab476c7f8abe1
mcgnly <katie@mcgnly.com>
2015-10-30 15:53:54 +0100

    consolidate email downcast, test for it, improve email validity regex

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,6 +57,13 @@ test "email validation should reject invalid addresses" do
     @user.save
     assert_not duplicate_user.valid?
   end
+  
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 
   test "password should be present (nonblank)" do
   @user.password = @user.password_confirmation = " " * 6
