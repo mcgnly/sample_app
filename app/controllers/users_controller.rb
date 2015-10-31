c98f3d52a1571c7aab3fc7de62980182fa3c8f8b
mcgnly <katie@mcgnly.com>
2015-10-31 15:34:49 +0100

    add "show" page to users controller and view add debugger box to users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    #debugger here would let the code jump into console mode in the server tab
+
+  end
+
 end
