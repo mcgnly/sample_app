a321e6f29da4285ca8cd1b311673d6fd09fb6603
mcgnly <katie@mcgnly.com>
2015-11-23 13:49:09 +0100

    add forget method to session helper and call it in logging out in session helper as well

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,8 +11,16 @@ module SessionsHelper
    cookies.permanent[:remember_token] = user.remember_token
  end
 
+ # Forgets a persistent session.
+ def forget(user)
+   user.forget
+   cookies.delete(:user_id)
+   cookies.delete(:remember_token)
+ end
+
  # Logs out the current user.
  def log_out
+   forget(current_user)
    session.delete(:user_id)
    @current_user = nil
  end
