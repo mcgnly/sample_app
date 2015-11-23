cc682caa2f252e1503bd4cd21b281062be7f827e
mcgnly <katie@mcgnly.com>
2015-11-23 16:24:18 +0100

    allow session to remember user based on the check box in the view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
       log_in user
       remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # Create an error message.
