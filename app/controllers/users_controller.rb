e6212bd346a383736648fb4e63cafda97d64a71b
mcgnly <katie@mcgnly.com>
2015-11-30 17:03:29 +0100

    add missing update function in user controller! oof.
    add a before filter to check that the correct users is trying to update things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -23,7 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -31,7 +34,6 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
 
 
   private
@@ -49,6 +51,12 @@ class UsersController < ApplicationController
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 
 end
