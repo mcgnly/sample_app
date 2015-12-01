3a2fce2b21651fd6c762de4951ea1d1ce6dfa323
mcgnly <katie@mcgnly.com>
2015-12-01 15:47:18 +0100

    add test that a not logged in person asking for the index gets sent back to the login pg add an index method to the user controller to spit back out all users in the DB as an instance var add the index method to the things that get caught up in the before-filter for being logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :index]
   before_action :correct_user,   only: [:edit, :update]
 
   def new
@@ -35,6 +35,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
 
   private
 
