6ce6a1e8adae4505a369d07ec9dcd3eb219b4dc7
mcgnly <katie@mcgnly.com>
2015-12-01 17:45:20 +0100

    add delete button to list of users on index only for admins add destroy function to users controller add the destroy fn to the logged-in before filter add a new before filter to check for if the user is an admin and only allow destroy to be used by them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def new
     @user = User.new
@@ -21,6 +22,12 @@ class UsersController < ApplicationController
       else
         render 'new'
       end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   def edit
@@ -62,5 +69,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+  
+  # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
